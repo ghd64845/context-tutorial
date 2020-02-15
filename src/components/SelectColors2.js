2cb9716e91280379d67c96c515f652156ebf4b95
LeeJaeHong <fleuve1008@gmail.com>
2020-02-16 00:20:00 +0900

    static contextType 사용

--- src/components/SelectColors2.js
+++ src/components/SelectColors2.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
+import ColorContext from "../contexts/Color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-class SelecColors2 extends Component {
+class SelectColors2 extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = color => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetSubcolor = subcolor => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+
   render() {
     return (
       <div>
         <h2>색상을 선택하세요.</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map(color => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "64px",
+                height: "64px",
+                cursor: "pointer"
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMemo={e => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
       </div>
     );
   }
 }
+
+export default SelectColors2;
