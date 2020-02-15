4b0c7635ca718c38abf14adf3a7389b87b4c4a1b
LeeJaeHong <fleuve1008@gmail.com>
2020-02-16 00:09:55 +0900

    useContext 활용

---
+++ src/components/SelectColors2.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+class SelecColors2 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+      </div>
+    );
+  }
+}
